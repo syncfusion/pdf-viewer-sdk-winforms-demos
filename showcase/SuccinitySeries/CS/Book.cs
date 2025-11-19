a4554de463c422f55aec02d936e60f9aa20c8d9b
Syncfusion <buildautomation@syncfusion.com>
2025-11-19 10:32:03 +0000

    v31.2.12

---
+++ showcase/SuccinitySeries/CS/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using Syncfusion.Windows.Forms.Tools;
+using Syncfusion.Windows.Forms;
+using System.ComponentModel;
+namespace SuccinitySeries
+{
+    class  Book:Control
+    {
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public string Name { get; set; }
+    }
+}
